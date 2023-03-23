39c4a16fe55f96ee0e65185cdeff4da4b4ffedbc
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-23 13:28:38 +0200

    Create Borrowing model and migrate, register it in admin panel

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import DO_NOTHING, Q, F
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(
+        Book, on_delete=DO_NOTHING, related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        User, on_delete=DO_NOTHING, related_name="borrowings"
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="borrow_date_before_return_date",
+                check=(
+                    Q(borrow_date__lte=F("expected_return_date"))
+                    & Q(borrow_date__lte=F("actual_return_date"))
+                ),
+            )
+        ]
