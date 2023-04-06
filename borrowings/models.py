62bcda85162947b3faa255060cb152564f2cf6a6
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-04-06 15:36:46 +0300

    Create Borrowing and Payment models

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import DO_NOTHING, Q, F
+from django.utils.translation import gettext_lazy as _
 
 from books.models import Book
 from users.models import User
@@ -26,3 +27,34 @@ class Borrowing(models.Model):
                 ),
             )
         ]
+
+    def __str__(self):
+        return (
+            f"Borrowing of {self.book.title} by {self.user.email} "
+            f"for {self.borrow_date} - {self.expected_return_date}"
+        )
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", _("Pending")
+        PAID = "PAID", _("Paid")
+
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", _("Payment")
+        FINE = "FINE", _("Fine")
+
+    status = models.CharField(max_length=7, choices=Status.choices)
+    type = models.CharField(max_length=7, choices=Type.choices)
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.DO_NOTHING, related_name="payments"
+    )
+    session_url = models.CharField(max_length=300)
+    session_id = models.CharField(max_length=300)
+    to_pay = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return (
+            f"{self.status}: {self.get_type_display()} of {self.to_pay} "
+            f"dollars for the {self.borrowing}"
+        )
