6400a38d0255979383c62af3a3db752ede62c071
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-04-06 15:59:15 +0300

    Create endpoints for BorrowingViewSet and PaymentViewSet

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from borrowings.views import BorrowingViewSet
+from borrowings.views import (
+    BorrowingViewSet,
+    PaymentViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("borrowings", BorrowingViewSet)
+router.register("payments", PaymentViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "borrowings"
