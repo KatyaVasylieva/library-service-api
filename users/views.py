b173a02f8365e07c2d2e9d2fb6ec301e78978816
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-04-18 16:21:20 +0300

    Format the code with black

--- users/views.py
+++ users/views.py
@@ -12,9 +12,13 @@ class CreateUserView(generics.CreateAPIView):
 
 
 @extend_schema_view(
-    get=extend_schema(description="Endpoint for getting a detailed info about the current user."),
+    get=extend_schema(
+        description="Endpoint for getting a detailed info about the current user."
+    ),
     put=extend_schema(description="Endpoint for updating the current user."),
-    patch=extend_schema(description="Endpoint for updating the current user partially."),
+    patch=extend_schema(
+        description="Endpoint for updating the current user partially."
+    ),
 )
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
